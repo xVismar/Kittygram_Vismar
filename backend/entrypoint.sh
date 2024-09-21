f96bf518b48df14808d47d0fe97df2ecba274644
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-09-21 11:59:41 +0300

    msg

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
-gunicorn backend.wsgi:aplication --bind 0.0.0.0:9000
+gunicorn backend.wsgi:application --bind 0.0.0.0:9000
