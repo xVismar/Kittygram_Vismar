13f41c4caef5bfadce65c392dff9a3ec29316c4a
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-09-21 03:23:43 +0300

    Compose changed , nginx- changed, settings.py updated, workflow updated

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -60,13 +60,14 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'NAME': os.getenv('POSTGRES_DB', 'default_db_name'),
+        'USER': os.getenv('POSTGRES_USER', 'default_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'default_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432')
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
