a85713d794fd6dca0a93a9943e184617fc9ddbf5
Aleksei Alekseev <blackcloudishere@gmail.com>
2024-09-22 14:11:19 +0300

    Обновление после первого ревью.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -58,21 +58,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'default_password'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432),
+        'PORT': os.getenv('DB_PORT', 5432)
     },
 
     'sqlite3': {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': BASE_DIR / 'db.sqlite3',
 }
-}
+}w
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
